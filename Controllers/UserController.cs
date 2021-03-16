eef44920f854cd4eaccf7db28f4f7d54c6feee67
DESKTOP-NV6T94D\Kuba <kuba.lobacz@gmail.com>
2021-03-16 01:52:02 +0100

    and UserController and dto

---
+++ Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿
+using ActivityTrackerApi.Data.DTOs;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ActivityTrackerApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public UserController(UserManager<IdentityUser> userManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Users()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RegisterUserDto user)
+        {
+            var userToRegister = _mapper.Map<IdentityUser>(user);
+            var result = await _userManager.CreateAsync(userToRegister, user.Password);
+
+            return Ok(result);
+        }
+
+    }
+}
