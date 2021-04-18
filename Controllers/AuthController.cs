ea2dcc67733971aed4d232087df411e991418688
DESKTOP-NV6T94D\Kuba <kuba.lobacz@gmail.com>
2021-04-18 21:01:57 +0200

    create auth controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,53 @@
+﻿using ActivityTrackerApi.Data.DTOs;
+using ActivityTrackerApi.Data.Models;
+using ActivityTrackerApi.Data.Repositories.Contracts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityTrackerApi.Controllers
+{
+    [ApiController]
+    [Route("/api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(UserManager<ApplicationUser> userManager, IConfiguration configuration )
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody]LoginRequestDto request)
+        {
+            var user = await _userManager.FindByNameAsync(request.Username);
+            if (user != null && await _userManager.CheckPasswordAsync(user, request.Password))
+            {
+                var key = _configuration["jwt-signing-key"];
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+                var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+                var tokenOptions = new JwtSecurityToken(
+                    issuer: "https://localhost:44301",
+                    audience: "https://localhost:44301",                    
+                    expires: DateTime.Now.AddHours(24),
+                    signingCredentials: signinCredentials
+                );
+
+               return Ok(new JwtSecurityTokenHandler().WriteToken(tokenOptions));
+            }
+
+            return BadRequest();
+        }
+    }
+}
