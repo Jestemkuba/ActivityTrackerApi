f0ad3990327e3f5ea329c79210af662768408321
DESKTOP-NV6T94D\Kuba <kuba.lobacz@gmail.com>
2021-04-23 21:38:04 +0200

    Create ActivityDto

---
+++ Data/DTOs/Activities/ActivityDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActivityTrackerApi.Data.DTOs.Activities
+{
+    public class ActivityDto
+    {
+        public string Name { get; set; }
+        public double Distance { get; set; }
+        public double MovingTime { get; set; }
+        public double ElapsedTime { get; set; }
+        public string Type { get; set; }
+        public double AverageSpeed { get; set; }
+        public double MaxSpeed { get; set; }
+    }
+}
